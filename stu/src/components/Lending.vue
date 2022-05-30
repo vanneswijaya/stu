567e94f93cd6547bae7a6ecc1555384c19499c78
Vannes Wijaya <vannesoey@gmail.com>
2022-05-31 03:08:51 +0800

    added lending dummies

--- stu/src/components/Lending.vue
+++ stu/src/components/Lending.vue
@@ -31,7 +31,7 @@
               1.12% interest rate
             </div>
             <div class="flex flex-row relative">
-              <div class="text-sm font-thin">Min. lend : HKD $1,000</div>
+              <div class="text-sm font-thin">Min. loan : HKD $1,000</div>
               <div class="text-sm font-thin absolute right-8">
                 Duration : 6 months
               </div>
@@ -44,26 +44,27 @@
           <div
             class="rounded-t-3xl bg-cover bg-center h-28 w-full"
             style="
-              background-image: url('https://s3.amazonaws.com/www.starterstory.com/story_images/images/000/001/636/original/open-uri20190519-4-1l5lbl7?1558272181');
+              background-image: url('https://coincentral.com/wp-content/uploads/2018/01/rentberry.png');
             "
           ></div>
           <div class="flex flex-col w-full m-3">
             <div class="text-base font-light leading-tight">
-              BeatBox: A Ready to Drink Cocktail with Unique Flavors
+              Rentberry: The first global home rental platform with full
+              automation!
             </div>
             <div class="text-sm font-thin mt-3">
-              Ronald Gustaf - Jakarta, Indonesia
+              Bill Bo - Sydney, Australia
             </div>
             <div class="text-sm font-light text-green-500">
-              8.5 credit score
+              7.7 credit score
             </div>
             <div class="text-sm font-light text-primary">
-              1.12% interest rate
+              1.01% interest rate
             </div>
             <div class="flex flex-row relative">
-              <div class="text-sm font-thin">Min. lend : HKD $1,000</div>
+              <div class="text-sm font-thin">Min. loan : HKD $2,000</div>
               <div class="text-sm font-thin absolute right-8">
-                Duration : 6 months
+                Duration : 3 months
               </div>
             </div>
           </div>
@@ -74,26 +75,26 @@
           <div
             class="rounded-t-3xl bg-cover bg-center h-28 w-full"
             style="
-              background-image: url('https://s3.amazonaws.com/www.starterstory.com/story_images/images/000/001/636/original/open-uri20190519-4-1l5lbl7?1558272181');
+              background-image: url('https://play-lh.googleusercontent.com/3ardMqXsc0ejfzDhXKjJsGF7BWt9ZxSlpUoceiPhCd0OyQJHma7DScu981ZFqHQW3jI');
             "
           ></div>
           <div class="flex flex-col w-full m-3">
             <div class="text-base font-light leading-tight">
-              BeatBox: A Ready to Drink Cocktail with Unique Flavors
+              Whimstay: Last-Minute Stays
             </div>
             <div class="text-sm font-thin mt-3">
-              Ronald Gustaf - Jakarta, Indonesia
+              Vincent Wijaya - Bandung, Indonesia
             </div>
             <div class="text-sm font-light text-green-500">
-              8.5 credit score
+              6.5 credit score
             </div>
             <div class="text-sm font-light text-primary">
-              1.12% interest rate
+              0.98% interest rate
             </div>
             <div class="flex flex-row relative">
-              <div class="text-sm font-thin">Min. lend : HKD $1,000</div>
+              <div class="text-sm font-thin">Min. loan : HKD $1,000</div>
               <div class="text-sm font-thin absolute right-8">
-                Duration : 6 months
+                Duration : 9 months
               </div>
             </div>
           </div>
