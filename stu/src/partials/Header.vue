47e632fe9661c370c540515163e051d9ccfc0aa0
Vannes Wijaya <vannesoey@gmail.com>
2022-06-03 17:42:18 +0800

    header fixed

--- stu/src/partials/Header.vue
+++ stu/src/partials/Header.vue
@@ -1,25 +1,28 @@
 <template>
-    <router-link to="/">
-        <div
-        class="flex flex-col h-16 w-full bg-dark text-white px-10 pt-4 overflow-auto"
-        >
-            <section>
-                <div class="flex justify-between items-center w-full">
-                    <img class="max-h-12 max-w-24" src="../assets/new-logo-stu-white.png" alt="new-logo-stu">
-                    <img class="w-10 h-10 rounded-full" src="../assets/profile_pic.jpg" alt="Rounded avatar">
-                </div>
-            </section>
+  <router-link to="/">
+    <div
+      class="flex flex-col h-21 w-full bg-dark text-white px-10 py-5 sticky top-0 z-50"
+    >
+      <section>
+        <div class="flex justify-between items-center w-full">
+          <img
+            class="max-h-12 max-w-24"
+            src="../assets/new-logo-stu-white.png"
+            alt="new-logo-stu"
+          />
+          <img
+            class="w-10 h-10 rounded-full"
+            src="../assets/profile_pic.jpg"
+            alt="Rounded avatar"
+          />
         </div>
-    </router-link>
-    
+      </section>
+    </div>
+  </router-link>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
